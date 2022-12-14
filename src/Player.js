932fe714216e2cdbebbb2c994fa86e963555aaa6
Neeraj TK <64883030+GaryGosh@users.noreply.github.com>
2022-12-14 14:16:47 +0530

    back btn integrated

--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "./Player.css";
+import BackIcon from "./assets/back-btn.png";
+import { useNavigate } from "react-router-dom";
 
 function Player() {
-  return <div>Player</div>;
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate("/", {});
+  };
+
+  return (
+    <div>
+      <div className="back-btn">
+        <img
+          className="back-icon"
+          src={BackIcon}
+          alt="back"
+          onClick={() => goBack()}
+        />
+      </div>
+      <div className="book"></div>
+    </div>
+  );
 }
 
 export default Player;
---
+++ src/assets/books/BlackSwan.js
@@ -0,0 +1,79 @@
+export const blackSwan = [
+  {
+    title: "CHAPTER 1",
+    mainBody: "What is a Black Swan Event ?",
+    type: "chapterTitle",
+    chapter: 1,
+    slide: 1,
+  },
+  {
+    title: "",
+    mainBody:
+      "For thousands of years, Europeans thought that all swans were White.",
+    type: "content",
+    chapter: 1,
+    slide: 2,
+  },
+  {
+    title: "",
+    mainBody: "No one had ever seen a swan that wasn't White.",
+    type: "content",
+    chapter: 1,
+    slide: 3,
+  },
+  {
+    title: "",
+    mainBody:
+      "They hadn't even considered the possibility that a swan could be any other color.",
+    type: "content",
+    chapter: 1,
+    slide: 4,
+  },
+  {
+    title: "",
+    mainBody:
+      "Then, one day in 1697, a Dutch explorer discovered a rare Black swan in Australia.",
+    type: "content",
+    chapter: 1,
+    slide: 5,
+  },
+  {
+    title: "",
+    mainBody:
+      "In an instant this widely accepted belief was proven completely false.",
+    type: "content",
+    chapter: 1,
+    slide: 6,
+  },
+  {
+    title: "",
+    mainBody:
+      "Sightings of millions of White swans over thousands of years had convinced Europeans that all the swans were White.",
+    type: "content",
+    chapter: 1,
+    slide: 7,
+  },
+  {
+    title: "",
+    mainBody: "It took only one Black Swan to prove them all wrong.",
+    type: "content",
+    chapter: 1,
+    slide: 8,
+  },
+  {
+    title: "",
+    mainBody:
+      "The lesson of the Black swan is that we dont know how much we dont know.",
+    type: "content",
+    chapter: 1,
+    slide: 9,
+  },
+  {
+    title: "",
+    mainBody:
+      "And there is always the possiblity of a change that will append our lives and beliefs.",
+    type: "content",
+    chapter: 1,
+    slide: 10,
+  },
+];
