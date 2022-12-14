9dd99c86e444170608d87265a8ec70081e5cb3d6
Neeraj TK <64883030+GaryGosh@users.noreply.github.com>
2022-12-14 12:38:44 +0530

    added player

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ import { BOOKS_LIBRARY } from "./constants/constants";
 function App() {
   return (
     <div className="home">
-      <h1 className="mural">Mural</h1>
+      <div className="mural">
+        <span className="m">M</span>
+        <span className="u">u</span>
+        <span className="r">r</span>
+        <span className="a">a</span>
+        <span className="l">l</span>
+      </div>
       <GoalBanner />
       <div className="home-carousel">
         {BOOKS_LIBRARY.map((book) => (
@@ -16,6 +22,22 @@ function App() {
             <BookBanner booksList={book} />
           </div>
         ))}
+      </div>
+      <div className="player-wrapper">
+        <div className="player">
+          <img
+            className="playing"
+            src="https://mural-assets.s3.ap-northeast-1.amazonaws.com/Black+Swan+playing.jpeg"
+            alt="playing"
+          />
+          <div className="player-content-wrapper">
+            <div className="title">The Black Swan</div>
+            <div className="continue">Continue Reading &gt;</div>
+          </div>
+        </div>
+        <div class="progress">
+          <div class="progress-value"></div>
+        </div>
       </div>
     </div>
   );
